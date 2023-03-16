5f161bcf58362aaee8e85789fb629d0d7ea441be
Arkadi Chubrik <arkadi@chubrik.org>
2023-03-17 01:52:17 +0400

    Documentation comments

--- Chubrik.Json/JsonNamingPolicies.cs
+++ Chubrik.Json/JsonNamingPolicies.cs
@@ -2,15 +2,39 @@
 
 using System.Text.Json;
 
+/// <summary>
+/// Determines the naming policies used to convert a string-based name to another format,
+/// such as camelCasing, snake_casing and kebab-casing formats.
+/// </summary>
 public static class JsonNamingPolicies
 {
+    /// <summary>
+    /// Gets the naming policy for camelCasing.
+    /// </summary>
+    /// <returns>The naming policy for camelCasing.</returns>
     public static JsonNamingPolicy CamelCase { get; } = JsonNamingPolicy.CamelCase;
 
+    /// <summary>
+    /// Gets the naming policy for kebab-lower-casing.
+    /// </summary>
+    /// <returns>The naming policy for kebab-lower-casing.</returns>
     public static JsonNamingPolicy KebabLowerCase { get; } = new JsonKebabLowerCaseNamingPolicy();
 
+    /// <summary>
+    /// Gets the naming policy for KEBAB-UPPER-CASING.
+    /// </summary>
+    /// <returns>The naming policy for KEBAB-UPPER-CASING.</returns>
     public static JsonNamingPolicy KebabUpperCase { get; } = new JsonKebabUpperCaseNamingPolicy();
 
+    /// <summary>
+    /// Gets the naming policy for snake_lower_casing.
+    /// </summary>
+    /// <returns>The naming policy for snake_lower_casing.</returns>
     public static JsonNamingPolicy SnakeLowerCase { get; } = new JsonSnakeLowerCaseNamingPolicy();
 
+    /// <summary>
+    /// Gets the naming policy for SNAKE_UPPER_CASING.
+    /// </summary>
+    /// <returns>The naming policy for SNAKE_UPPER_CASING.</returns>
     public static JsonNamingPolicy SnakeUpperCase { get; } = new JsonSnakeUpperCaseNamingPolicy();
 }
